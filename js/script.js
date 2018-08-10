bee52d7db7da71fec8dd4f844957a0b3c138181f
brian kimani <briankimani013@gmail.com>
2018-08-10 15:33:10 +0300

    index.html, Images, css and js directories

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var a = parseInt(prompt("Enter side A"));
+var b = parseInt(prompt("Enter side B"));
+var c = parseInt(prompt("Enter side C"));
+
+if (a === b && b === c && c === a) {
+    alert("This is an Equilateral Triangle.")
+} else if (a === b || b === c || a === c) {
+    alert("This is an Isoscles Triangle.")
+} else if (a + b <= c || b + c <= a || a + c <= b) {
+    alert("Not a Triangle");
+}
